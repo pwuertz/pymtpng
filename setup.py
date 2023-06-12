127089abc308f2b010b062623e65b1f29f9d6399
Peter Würtz <pwuertz@gmail.com>
2023-06-13 00:19:11 +0200

    build: Update CI build and PYPI publish

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import find_packages, setup
+import pathlib
+from setuptools import setup
 from cmake_build_extension import BuildExtension, CMakeExtension
+
+this_directory = pathlib.Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+long_description_content_type="text/markdown"
 
 setup(
     name="pymtpng",
     version="1.0",
     author="Peter Würtz",
+    author_email="pwuertz@gmail.com",
+    url="https://github.com/pwuertz/pymtpng",
     description="Python bindings for MTPNG library",
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
     ext_modules=[CMakeExtension(
         name="pymtpng",
         source_dir=".",
